78fabbbd248a912ce491b58c16a9dad210838378
Daniel Ruatta <druatta@gmail.com>
2020-01-16 22:01:13 -0800

    Yeeeee
    Started assignment one

---
+++ Assignments/AssignmentOne/src/AssignmentOne.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : AssignmentOne.c
+ Author      : Daniel Ruatta
+ Version     :
+ Copyright   : Your copyright notice is invalid
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Baby shark doo doo doo");
+	return EXIT_SUCCESS;
+}
